f2f9346e9bbb5e2c78c3dcf9f3d74a479a294f5c
Patrik <matepatt@gmail.com>
2019-05-08 13:57:27 +0200

    start

---
+++ src/main/java/com/example/c_beadando_f3sz3g/CBeadandoF3sz3gApplication.java
@@ -0,0 +1,13 @@
+package com.example.c_beadando_f3sz3g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CBeadandoF3sz3gApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CBeadandoF3sz3gApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/c_beadando_f3sz3g/CBeadandoF3sz3gApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.c_beadando_f3sz3g;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CBeadandoF3sz3gApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
